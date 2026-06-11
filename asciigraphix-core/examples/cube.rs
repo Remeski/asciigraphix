d3576eec2f0d62a0eef66a6aabe6c88b946f3ffa
Remesk1 <eeliremes@gmail.com>
2026-06-11 20:01:21 +0300

    these are for debugging

--- asciigraphix-core/examples/cube.rs
+++ asciigraphix-core/examples/cube.rs
@@ -1,23 +1,37 @@
-use asciigraphix_core::{Display, math::Point, shapes::Shape};
+use std::io::Read;
+
+use asciigraphix_core::{
+    Display,
+    math::Point,
+    shapes::{Face, Shape},
+};
 
 fn main() {
-    const DISPLAY_X: usize = 100;
-    const DISPLAY_Y: usize = 50;
+    const DISPLAY_X: usize = 200;
+    const DISPLAY_Y: usize = 100;
     let mut display = Display::new(
         DISPLAY_X,
         DISPLAY_Y,
-        Point(0.0, 0.0, 10.0),
+        Point(0.0, 0.0, 6.0),
         Point(0.0, 0.0, -1.0),
-        3.14 / 2.0,
+        3.14 / 3.0,
     );
-    let mut cube = Shape::generate_cube_colorful(Point(0.0, 0.0, 0.0), 5.0);
+
+
+    let mut cube = Shape::generate_cube_colorful(Point(0.0, 0.0, 0.0), 4.0);
+    // cube.faces = vec![cube.faces[0..4].to_vec(), cube.faces[6..8].to_vec()]
+    //     .concat()
+    //     .to_vec();
 
     Display::clear_screen();
+    let mut buf = [0u8; 1];
     loop {
         cube.rotate(
             &cube.center.clone().unwrap_or(Point(0.0, 0.0, 0.0)),
-            (0.05, 0.00, 0.0),
+            (0.09, 0.01, 0.00),
         );
+
+        // std::io::stdin().read(&mut buf).unwrap();
 
         display.render_terminal(&cube);
 
--- asciigraphix-core/examples/face.rs
+++ asciigraphix-core/examples/face.rs
@@ -1,5 +1,9 @@
+use std::io::Read;
+
 use asciigraphix_core::{
-    Display, math::Point, shapes::{Face, Shape}
+    Display,
+    math::Point,
+    shapes::{Face, Shape},
 };
 
 fn main() {
@@ -10,22 +14,38 @@ fn main() {
         DISPLAY_Y,
         Point(0.0, 0.0, 2.0),
         Point(0.0, 0.0, -1.0),
-        3.14 / 2.0,
+        3.14 / 3.0,
     );
-    let shape = Shape::new(
+    // let shape = Shape::new(
+    //     vec![
+    //         Point(-3.0, 0.0, 0.0),
+    //         Point(2.0, 2.0, 0.0),
+    //         Point(2.0, -2.0, 0.0),
+    //     ],
+    //     vec![],
+    //     vec![Face::new(0,1,2)],
+    // );
+    let mut shape = Shape::new(
         vec![
             Point(0.0, 0.0, 0.0),
-            Point(1.0, 0.0, 0.0),
-            Point(1.0, 1.0, 0.0),
-            Point(0.0, 1.0, 0.0),
+            Point(2.0, 1.0, 0.0),
+            Point(1.0, 2.0, 0.0),
         ],
         vec![],
-        vec![Face::new(2,3,0), Face::new(0,1,2)],
+        vec![Face::new(0, 1, 2)],
     );
     // let mut cube = Shape::generate_cube(Point(0.0, 0.0, 0.0), 1.0);
     // cube.faces = vec![Face(0, 1, 2)];
 
-    display.render_print(&shape);
+    // display.render_print(&shape.combine(&shape2));
+    Display::clear_screen();
+    let mut buf = [0u8; 1];
+    // loop {
+        // shape.rotate(&Point(0.0, 0.0, 0.0), (0.1, 0.0, 0.0));
+        display.render_print(&shape);
 
-    std::thread::sleep(std::time::Duration::from_millis(33));
+        // std::io::stdin().read(&mut buf).unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(33));
+    // }
 }
